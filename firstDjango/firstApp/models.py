cd0213808b0ff76733494c2bef9e63247c9a9832
MunishUpadhyay <munishupadhyay183@gmail.com>
2026-03-28 23:58:03 +0530

    latest commit

--- firstDjango/firstApp/models.py
+++ firstDjango/firstApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class chaiVariety(models.Model):
+    CHAI_CHOICE_TYPE = [
+        ('ML', 'Masala Chai'),
+        ('GR', 'Ginger Chai'),
+        ('KL', 'Kashmiri Chai'),
+        ('PL', 'Plain Chai'),
+        ('EL', 'Elaichi Chai'),
+    ]
+
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='/images')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_CHOICE_TYPE)
